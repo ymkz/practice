6073ee691f263d6d53cff41f8ff5e19b011b0341
ymkz <ymkz@users.noreply.github.com>
2024-01-09 21:38:00 +0900

    chore: rename HEALTHCHECK_UP

--- nextjs-pages-custom-server/server/healthcheck.mjs
+++ nextjs-pages-custom-server/server/healthcheck.mjs
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 export const healthcheckHandler = Router().get('/health', (_, res) => {
-  if (process.env.HEALTHCHECK_OK !== 'true') {
+  if (process.env.HEALTHCHECK_UP !== 'true') {
     return res.status(503).send('DOWN')
   }
   return res.status(200).send('UP')
